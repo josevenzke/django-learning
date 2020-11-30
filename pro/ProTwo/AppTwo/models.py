bea7ac8512f7cc4ad43e529d3d40fa876b8aab87
josevenzke <josevenzkejr@gmail.com>
2020-11-30 11:31:05 -0300

    Models variable modification

--- pro/ProTwo/AppTwo/models.py
+++ pro/ProTwo/AppTwo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    us_fname = models.CharField(max_length=264)
-    us_lname = models.CharField(max_length=264)
-    us_email = models.EmailField(max_length=264)
+    first_name = models.CharField(max_length=264)
+    last_name = models.CharField(max_length=264)
+    email = models.EmailField(max_length=264)
 
     def __str__(self):
-        return self.us_fname+" "+self.us_lname
+        return self.first_name+" "+self.last_name
